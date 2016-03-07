d9276734f190fae01813ca77f7b32ceb52807f17
Xellss <Xellsnr1@hotmail.de>
2016-03-07 01:49:33 +0100

    OMMIT:

---
+++ UnityFlocking/Assets/MoveTo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform Goal;
+    private NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = Goal.position;
+    }
+
+    void Update()
+    {
+          agent.destination = Goal.position;
+    }
+
+}
